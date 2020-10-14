de0175643ff7950d5411883234c768cf4d95a494
fredericoliz <fredericoliz25@gmail.com>
2020-10-14 00:26:26 -0300

    Contorno da célula funcionando.

--- WindowsFormsApp1/Framework.Designer.cs
+++ WindowsFormsApp1/Framework.Designer.cs
@@ -28,35 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.buttonProcess = new System.Windows.Forms.Button();
             this.buttonLoad = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.MononucleadasCells = new System.Windows.Forms.Label();
+            this.HealthyCells = new System.Windows.Forms.Label();
+            this.CellsQuantity = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Y_pos = new System.Windows.Forms.Label();
+            this.X_pos = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(475, 59);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Analisador de Núcleos";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox1.Controls.Add(this.buttonProcess);
             this.groupBox1.Controls.Add(this.buttonLoad);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.groupBox1.Location = new System.Drawing.Point(0, 59);
+            this.groupBox1.Location = new System.Drawing.Point(8, 77);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(364, 662);
+            this.groupBox1.Size = new System.Drawing.Size(356, 592);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
@@ -64,7 +66,7 @@
             // 
             this.buttonProcess.Location = new System.Drawing.Point(6, 135);
             this.buttonProcess.Name = "buttonProcess";
-            this.buttonProcess.Size = new System.Drawing.Size(351, 56);
+            this.buttonProcess.Size = new System.Drawing.Size(344, 56);
             this.buttonProcess.TabIndex = 1;
             this.buttonProcess.Text = "Analisar Imagem";
             this.buttonProcess.UseVisualStyleBackColor = true;
@@ -74,45 +76,174 @@
             // 
             this.buttonLoad.Location = new System.Drawing.Point(6, 41);
             this.buttonLoad.Name = "buttonLoad";
-            this.buttonLoad.Size = new System.Drawing.Size(352, 57);
+            this.buttonLoad.Size = new System.Drawing.Size(344, 57);
             this.buttonLoad.TabIndex = 0;
             this.buttonLoad.Text = "Carregar Imagem";
             this.buttonLoad.UseVisualStyleBackColor = true;
             this.buttonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.MononucleadasCells);
+            this.groupBox2.Controls.Add(this.HealthyCells);
+            this.groupBox2.Controls.Add(this.CellsQuantity);
+            this.groupBox2.Location = new System.Drawing.Point(1074, 77);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(379, 595);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // MononucleadasCells
+            // 
+            this.MononucleadasCells.AutoSize = true;
+            this.MononucleadasCells.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MononucleadasCells.Location = new System.Drawing.Point(6, 150);
+            this.MononucleadasCells.Name = "MononucleadasCells";
+            this.MononucleadasCells.Size = new System.Drawing.Size(237, 41);
+            this.MononucleadasCells.TabIndex = 2;
+            this.MononucleadasCells.Text = "Mononucleadas:";
+            // 
+            // HealthyCells
+            // 
+            this.HealthyCells.AutoSize = true;
+            this.HealthyCells.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HealthyCells.Location = new System.Drawing.Point(6, 87);
+            this.HealthyCells.Name = "HealthyCells";
+            this.HealthyCells.Size = new System.Drawing.Size(156, 41);
+            this.HealthyCells.TabIndex = 1;
+            this.HealthyCells.Text = "Saudáveis:";
+            // 
+            // CellsQuantity
+            // 
+            this.CellsQuantity.AutoSize = true;
+            this.CellsQuantity.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CellsQuantity.Location = new System.Drawing.Point(6, 25);
+            this.CellsQuantity.Name = "CellsQuantity";
+            this.CellsQuantity.Size = new System.Drawing.Size(232, 41);
+            this.CellsQuantity.TabIndex = 0;
+            this.CellsQuantity.Text = "Total de Células:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.AutoSize = true;
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Location = new System.Drawing.Point(371, 77);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(694, 643);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pictureBox1.Location = new System.Drawing.Point(364, 59);
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 25);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(642, 662);
-            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.Size = new System.Drawing.Size(688, 592);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(8, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1445, 66);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(485, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(475, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Analisador de Núcleos";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.Y_pos);
+            this.panel2.Controls.Add(this.X_pos);
+            this.panel2.Location = new System.Drawing.Point(8, 678);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1445, 31);
+            this.panel2.TabIndex = 6;
+            // 
+            // Y_pos
+            // 
+            this.Y_pos.AutoSize = true;
+            this.Y_pos.Location = new System.Drawing.Point(68, 11);
+            this.Y_pos.Name = "Y_pos";
+            this.Y_pos.Size = new System.Drawing.Size(20, 20);
+            this.Y_pos.TabIndex = 1;
+            this.Y_pos.Text = "Y:";
+            // 
+            // X_pos
+            // 
+            this.X_pos.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.X_pos.AutoSize = true;
+            this.X_pos.Location = new System.Drawing.Point(0, 11);
+            this.X_pos.Name = "X_pos";
+            this.X_pos.Size = new System.Drawing.Size(21, 20);
+            this.X_pos.TabIndex = 0;
+            this.X_pos.Text = "X:";
+            this.X_pos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Framework
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1006, 721);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1465, 721);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label1);
             this.Name = "Framework";
             this.Text = "Nuclei Detector";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonLoad;
         private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label Y_pos;
+        private System.Windows.Forms.Label X_pos;
+        private System.Windows.Forms.Label MononucleadasCells;
+        private System.Windows.Forms.Label HealthyCells;
+        private System.Windows.Forms.Label CellsQuantity;
     }
 }
 
--- WindowsFormsApp1/Framework.cs
+++ WindowsFormsApp1/Framework.cs
@@ -8,19 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.Util;
+using Emgu.CV.Features2D;
 using Emgu.CV.Structure;
-using Emgu.CV.ImgHash;
+using Emgu.CV.Util;
+using Emgu.CV.Flann;
+using Emgu.CV.Cuda;
+using System.Runtime.InteropServices;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.CV.XFeatures2D;
 
 namespace WindowsFormsApp1
 {
     public partial class Framework : Form
     {
         public string imagePath;
-        Image<Bgr, Byte> blueChannel;
-        Image<Gray, Byte> img;
-        Image<Gray, Byte> dst;
+        public int CelulasIgnoradas;
+        public int CelulasNormais;
+        public int CelulasMicronucleadas;
+        public int Background;
+        Image<Hsv, Byte> img;
+        Image<Hsv, Byte> Camera;
+        Image<Hsv, Byte> CameraResult;
+        Image<Hsv, Byte> imgH;
 
         public Framework()
         {
@@ -41,60 +51,139 @@ namespace WindowsFormsApp1
             }
         }
 
-        private void buttonProcess_Click(object sender, EventArgs e)
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            //chama a imagem carregada
-            img = new Image<Gray, Byte>(imagePath);
-            blueChannel = new Image<Bgr, Byte>(imagePath);
-            Suppress(0);
-
-            //converte para o canal azul da imagem
-            //img = blueChannel.Convert<Gray, Byte>();
-            dst = new Image<Gray, Byte>(imagePath);
-
-            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
-            Mat hierarchy = new Mat();
-
-            //setando os filtros para encontrar as celulas
-            CvInvoke.MedianBlur(img, dst, 21);
-            CvInvoke.Threshold(dst, dst, 135, 255, ThresholdType.Binary);
-            
-            //encontro o contorno das celulas
-            CvInvoke.FindContours(dst, contours, hierarchy, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
-            //CvInvoke.DrawContours(img, contours, -1, new MCvScalar(255, 0, 0));
-
-            // criando uma mascara
-            //Mat msk = new Mat(imagePath, ImreadModes.Grayscale);
-            Image<Gray, Byte> mask = new Image<Gray, Byte>(img.Cols, img.Rows);
-            Image<Gray, Byte> separateCell;
-            mask.SetZero();
-            CvInvoke.DrawContours(mask, contours, 1, new MCvScalar(255, 255, 255), -1);
-            separateCell = img & mask;
-
-
-            //RESULTADO NO OPENCV/
-            String win1 = "Test Window"; //The name of the window
-            CvInvoke.NamedWindow(win1); //Create the window using the specific name
-            CvInvoke.Imshow(win1,separateCell); //Show the image
-            CvInvoke.WaitKey(0);  //Wait for the key pressing event
-            CvInvoke.DestroyWindow(win1); //Destroy the window if key is pressed
-        }
-
-
-        private void Suppress(int spectrum)
-        {
-
-            for (int i = 0; i < img.Height; i++)
+            if (pictureBox1.Image != null)
             {
-                for (int j = 0; j < img.Width; j++)
-                {
-                    blueChannel.Data[i, j, spectrum] = 0;
-                }
+                X_pos.Text = "X: " + e.X.ToString();
+                Y_pos.Text = "Y: " + e.Y.ToString();
             }
         }
 
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null) return;
+                CelulasMicronucleadas = 0;
+                CelulasNormais = 0;
+                CelulasIgnoradas = 0;
+                Background = 0;
+                SimpleBlobDetector param = new SimpleBlobDetector();
+                VectorOfKeyPoint keypoint = new VectorOfKeyPoint();
+                img = new Image<Hsv, Byte>(imagePath);
+                Camera = new Image<Hsv, Byte>(imagePath);
+                CameraResult = new Image<Hsv, Byte>(imagePath);
+                Matrix<byte> kernel1 = new Matrix<byte>(new Byte[3, 3] { { 1, 1, 1}, { 1, 1, 1}, { 1, 1, 1} });
+                var mask = img[0].MorphologyEx(MorphOp.Open, kernel1, new Point(-1, -1), 1, BorderType.Default, new MCvScalar())
+                    .MorphologyEx(MorphOp.Close, kernel1, new Point(-1, -1), 1, BorderType.Default, new MCvScalar());
+                CvInvoke.Threshold(mask, mask, 50, 255, ThresholdType.Otsu);
+                Mat distanceTransform = new Mat();
+                CvInvoke.DistanceTransform(mask, distanceTransform, null, Emgu.CV.CvEnum.DistType.L2, 5);
+                CvInvoke.Normalize(distanceTransform, distanceTransform, 0, 255, NormType.MinMax);
+                var markers = distanceTransform.ToImage<Gray, byte>()
+                    .ThresholdBinary(new Gray(90), new Gray(255));
+                CvInvoke.ConnectedComponents(markers, markers);
+                var finalMarkers = markers.Convert<Gray, Int32>();
+                CvInvoke.Watershed(img, finalMarkers);
+                Image<Gray, byte> boundaries = finalMarkers.Convert<byte>(delegate (Int32 x)
+                {
+                    return (byte)(x == -1 ? 255 : 0);
+                });
 
+                boundaries._Dilate(1);
+                CvInvoke.Threshold(boundaries, boundaries, 0, 255, ThresholdType.BinaryInv);
+                Image<Gray, Byte> CellsBoundaries = new Image<Gray, Byte>(img.Cols, img.Rows);
+                CellsBoundaries.SetZero();
+                CellsBoundaries = boundaries & mask;
+                // criando a mascara dos nucleos:
+                imgH = new Image<Hsv, Byte>(imagePath);
+                var nucleimask = imgH[0];
+                CvInvoke.Threshold(nucleimask, nucleimask, 100, 255, ThresholdType.Binary);
+                CvInvoke.MorphologyEx(nucleimask,nucleimask,MorphOp.Close, kernel1, new Point(-1, -1), 1, BorderType.Default, new MCvScalar());
 
+                ////////////////////////////////////////
+                // aqui cria o contorno do watershed e separa o nucleo de cada celula encontrada
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat hierarchy = new Mat();
+                CvInvoke.FindContours(CellsBoundaries, contours, hierarchy, RetrType.External, ChainApproxMethod.ChainApproxNone);
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    Image<Hsv, Byte> separateCell = new Image<Hsv, Byte>(img.Cols, img.Rows);
+                    Image<Hsv, Byte> separateNuclei = new Image<Hsv, Byte>(img.Cols, img.Rows);
+                    Image<Hsv, Byte> maskCell = new Image<Hsv, Byte>(img.Cols, img.Rows);
+                    //Image<Hsv, Byte> ImageSave = new Image<Gray, Byte>(img.Cols, img.Rows);
+                    //MessageBox.Show(contours.Size.ToString());
+                    maskCell.SetZero();
+                    separateCell.SetZero();
+                    separateNuclei.SetZero();
+                    double area = CvInvoke.ContourArea(contours[i]);
+                    //cria a mascara para separar cada célula e seus núcleos
+                    //caso o contorno for a area total da foto, ignora
+                    if (area < 500)
+                    {
+                        Background += 1;
+                    }
+                    //se a celula for muito grande é uma célula a ser ignorada
+                    else if (area > 4000 || area < 1000) 
+                    {
+                        CvInvoke.DrawContours(CameraResult, contours, i, new MCvScalar(255, 100, 255));
+                        CelulasIgnoradas += 1;
 
+                    }
+                    //se tudo estiver correto, realiza a análise do núcleo da célula
+                    else
+                    {
+                        CvInvoke.DrawContours(maskCell, contours, i, new MCvScalar(255, 255, 255), -1);
+                        separateCell = Camera & maskCell;
+                        separateNuclei = separateCell & nucleimask.Convert<Hsv, Byte>();
+                        VectorOfVectorOfPoint Nucleicontours = new VectorOfVectorOfPoint();
+                        Mat Nucleihierarchy = new Mat();
+                        CvInvoke.FindContours(separateNuclei.Convert<Gray, Byte>(), Nucleicontours, Nucleihierarchy, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
+                        //se encontrar 2 núcleos dentro da célula
+                        if (Nucleicontours.Size == 2)
+                        {
+                            double AreaFirstNuclei = CvInvoke.ContourArea(Nucleicontours[0]);
+                            double AreaSecondNuclei = CvInvoke.ContourArea(Nucleicontours[1]);
+                            //ignora os nucleos gigantes por conta do threshold
+                            if (AreaFirstNuclei > 50 || AreaSecondNuclei > 50) 
+                            {
+                                CvInvoke.DrawContours(CameraResult, contours, i, new MCvScalar(255, 100, 255));
+                                CelulasIgnoradas += 1;
+                            }
+                            //se for muito menor (1/5 do valor) que o primeiro nucleo ou vice versa, é um micronucleo
+                            else if ((AreaFirstNuclei < (AreaSecondNuclei / 2)) || (AreaSecondNuclei < (AreaFirstNuclei / 2)))
+                            {
+                                CelulasMicronucleadas += 1;
+                                CvInvoke.DrawContours(CameraResult, Nucleicontours, -1, new MCvScalar(60, 255, 255), -1);
+                                CvInvoke.DrawContours(CameraResult, contours, i, new MCvScalar(60, 255, 255));
+                            }
+                        }
+                        //se tiver apenas um núcleo dentro da célula
+                        else if (Nucleicontours.Size == 1)
+                        {
+                            CelulasNormais += 1;
+                            CvInvoke.DrawContours(CameraResult, Nucleicontours, -1, new MCvScalar(128, 128, 128), -1);
+                            CvInvoke.DrawContours(CameraResult, contours, i, new MCvScalar(128, 128, 128));
+                        }
+                        else
+                        {
+                            CvInvoke.DrawContours(CameraResult, contours, i, new MCvScalar(0, 0, 255));
+                            CvInvoke.DrawContours(CameraResult, Nucleicontours, -1, new MCvScalar(0, 0, 255),-1);
+                        }
+                    }
+                }
+                HealthyCells.Text = "Células Normais: " + CelulasNormais.ToString();
+                MononucleadasCells.Text = "Micronucleadas: " + CelulasMicronucleadas.ToString();
+                CellsQuantity.Text = "Ignoradas: " + CelulasIgnoradas.ToString();
+                pictureBox1.Image = CameraResult.ToBitmap();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
     }
 }
